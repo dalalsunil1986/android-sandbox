ef78ddd4c78dcc1186c50c7692ea3da503cd8453
Benjamin Winterberg <winterbe@googlemail.com>
2010-12-28 16:18:44 +0100

    rotating circle

--- src/de/winterberg/android/sandbox/sample1/Sample1Activity.java
+++ src/de/winterberg/android/sandbox/sample1/Sample1Activity.java
@@ -1,7 +1,6 @@
 package de.winterberg.android.sandbox.sample1;
 
 import android.app.Activity;
-import android.graphics.PixelFormat;
 import android.os.Bundle;
 
 /**
@@ -12,8 +11,6 @@ public class Sample1Activity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(new Sample1View(this));
-        getWindow().setFormat(PixelFormat.RGBA_8888);
-//        getWindow().setFlags(WindowManager.LayoutParams.FLAG_DITHER, WindowManager.LayoutParams.FLAG_DITHER);
     }
 
 }
--- src/de/winterberg/android/sandbox/sample1/Sample1View.java
+++ src/de/winterberg/android/sandbox/sample1/Sample1View.java
@@ -3,6 +3,9 @@ package de.winterberg.android.sandbox.sample1;
 import android.content.Context;
 import android.graphics.*;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.RotateAnimation;
 
 /**
  * @author Benjamin Winterberg
@@ -11,20 +14,36 @@ public class Sample1View extends View {
 
     private static final String QUOTE = "Now is the time for all good men to come to the aid of their country.";
 
+    private Animation animation;
+
     public Sample1View(Context context) {
         super(context);
     }
 
+    private void initAnimation(Canvas canvas) {
+        animation = new RotateAnimation(0, 360, 150, 150);
+        animation.setRepeatCount(Animation.INFINITE);
+        animation.setRepeatMode(Animation.RESTART);
+        animation.setDuration(7500L);
+        animation.setInterpolator(new LinearInterpolator());
+        startAnimation(animation);
+    }
+
     @Override
     protected void onDraw(Canvas canvas) {
-        int circleHeight = 150;
-        int circleWidth = 150;
+        if (animation == null)
+            initAnimation(canvas);
+
+        int x = 150;
+        int y = 150;
+        int r = 100;
 
         Paint glow = new Paint();
+        glow.setAlpha(255);
         glow.setShadowLayer(40, 0, 0, Color.argb(200, 255, 0, 0));
 
         Path glowCircle = new Path();
-        glowCircle.addCircle(circleWidth, circleHeight, 100, Path.Direction.CW);
+        glowCircle.addCircle(x, y, r, Path.Direction.CW);
         canvas.drawPath(glowCircle, glow);
 
 
@@ -35,18 +54,19 @@ public class Sample1View extends View {
 
         int color1 = Color.rgb(60, 60, 60);
         int color2 = Color.rgb(200, 200, 200);
-        LinearGradient gradient = new LinearGradient(0, 0, 0, circleHeight*2, color1, color2, Shader.TileMode.REPEAT);
+        LinearGradient gradient = new LinearGradient(0, 0, 0, y*2, color2, color1, Shader.TileMode.REPEAT);
         cPaint.setShader(gradient);
 
 
         Paint tPaint = new Paint();
         tPaint.setTextSize(18f);
+
         tPaint.setTypeface(Typeface.DEFAULT_BOLD);
         tPaint.setColor(Color.BLACK);
         tPaint.setAntiAlias(true);
 
         Path circle = new Path();
-        circle.addCircle(circleWidth, circleHeight, 100, Path.Direction.CW);
+        circle.addCircle(x, y, r, Path.Direction.CW);
 
         canvas.drawPath(circle, cPaint);
         canvas.drawTextOnPath(QUOTE, circle, 0, 20, tPaint);
