0799b93b4f8ba0a4c52f50b626a02c62b3596c0d
Benjamin Winterberg <winterbe@googlemail.com>
2010-12-29 18:48:45 +0100

    keep bouncing up'n'down

--- src/de/winterberg/android/sandbox/sample2/BallView.java
+++ src/de/winterberg/android/sandbox/sample2/BallView.java
@@ -5,7 +5,12 @@ import android.graphics.*;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
 import android.view.animation.TranslateAnimation;
+import de.winterberg.android.sandbox.util.AnimationListenerAdapter;
+
+import java.util.Random;
 
 import static de.winterberg.android.sandbox.sample2.Constants.*;
 
@@ -15,16 +20,16 @@ import static de.winterberg.android.sandbox.sample2.Constants.*;
 public class BallView extends View {
     private static final String TAG = "Sample2";
 
-//    private TranslateAnimation animation;
-//
-//    private PointF start;
-//    private PointF end;
+    private static final Random RANDOM = new Random();
+
+    private boolean initialized = false;
 
     private RectF bounds;
 
     private PointF position;
 
-    private boolean initialized = false;
+    private PointF nextPosition;
+
 
     public BallView(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -32,33 +37,84 @@ public class BallView extends View {
 
     private void initialize(Canvas canvas) {
         // calculate inner bounds
-        float left = MARGIN + STROKE_WIDTH;
-        float top = MARGIN + STROKE_WIDTH;
-        float right = canvas.getWidth() - MARGIN - STROKE_WIDTH;
-        float bottom = canvas.getHeight() - MARGIN - STROKE_WIDTH;
+        float left = MARGIN + STROKE_WIDTH + CIRCLE_RADIUS;
+        float top = MARGIN + STROKE_WIDTH + CIRCLE_RADIUS;
+        float right = canvas.getWidth() - MARGIN - STROKE_WIDTH - CIRCLE_RADIUS;
+        float bottom = canvas.getHeight() - MARGIN - STROKE_WIDTH - CIRCLE_RADIUS;
         bounds = new RectF(left, top, right, bottom);
 
         // setup starting position
-        position = new PointF(canvas.getWidth() / 2f, bottom - CIRCLE_RADIUS);
-
-        // ready for take off
-        doAnimate(position, new PointF(canvas.getWidth() / 2f, top + CIRCLE_RADIUS));
+        doAnimate(canvas);
 
         // done
         initialized = true;
     }
 
-    private void doAnimate(PointF from, PointF to) {
-        float toYDelta = to.y - from.y;
-        float fromYDelta = 0;
+    private void doAnimate(Canvas canvas) {
+        position = new PointF(canvas.getWidth() / 2f, bounds.bottom);
+        nextPosition = new PointF(canvas.getWidth() / 2f, bounds.top);
+        doAnimate(position, nextPosition);
+    }
+
+//    private PointF position(float x, float y) {
+//        float xx = x;
+//        if (x == bounds.left)
+//            xx += CIRCLE_RADIUS;
+//        if (x == bounds.right)
+//            xx -= CIRCLE_RADIUS;
+//
+//        float yy = y;
+//        if (y == bounds.top)
+//            yy += CIRCLE_RADIUS;
+//        if (y == bounds.bottom)
+//            yy -= CIRCLE_RADIUS;
+//
+//        return new PointF(xx, yy);
+//    }
+
+    private void doAnimate(final PointF from, final PointF to) {
+        position = from;
+        nextPosition = to;
+
         float toXDelta = to.x - from.x;
+        float toYDelta = to.y - from.y;
         float fromXDelta = 0;
-        Log.d(TAG, "Starting animation: fromYDelta=" + fromYDelta + "; toYDelta=" + toYDelta + "; fromXDelta="
-                + fromXDelta + "; toXDelta=" + toXDelta);
-        TranslateAnimation animation = new TranslateAnimation(fromXDelta, toXDelta, fromYDelta, toYDelta);
-        animation.setDuration(2500);
-        animation.setFillAfter(true);
+        float fromYDelta = 0;
+
+        Log.d(TAG, "fromYDelta=" + fromYDelta + "; toYDelta=" + toYDelta
+                + "; fromXDelta=" + fromXDelta + "; toXDelta=" + toXDelta);
+
+        final TranslateAnimation animation = createAnimation(fromXDelta, toXDelta, fromYDelta, toYDelta);
+        animation.setAnimationListener(new AnimationListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                position = to;
+                nextPosition = null;
+                doAnimate(position, nextRandomPosition(position));
+            }
+        });
         startAnimation(animation);
+    }
+
+    private PointF nextRandomPosition(PointF position) {
+        int xBounds = (int) (bounds.right - bounds.left);
+        float x = RANDOM.nextInt(xBounds) + bounds.left;
+
+        float y;
+        if (position.y <= bounds.top)
+            y = bounds.bottom;
+        else
+            y = bounds.top;
+
+        return new PointF(x, y);
+    }
+
+    private TranslateAnimation createAnimation(float fromXDelta, float toXDelta, float fromYDelta, float toYDelta) {
+        TranslateAnimation animation = new TranslateAnimation(fromXDelta, toXDelta, fromYDelta, toYDelta);
+        animation.setDuration(1000);
+        animation.setFillAfter(true);
+        animation.setInterpolator(new LinearInterpolator());
+        return animation;
     }
 
     @Override
---
+++ src/de/winterberg/android/sandbox/util/AnimationListenerAdapter.java
@@ -0,0 +1,20 @@
+package de.winterberg.android.sandbox.util;
+
+import android.view.animation.Animation;
+
+/**
+ * @author Benjamin Winterberg
+ */
+public class AnimationListenerAdapter implements Animation.AnimationListener {
+    public void onAnimationStart(Animation animation) {
+
+    }
+
+    public void onAnimationEnd(Animation animation) {
+
+    }
+
+    public void onAnimationRepeat(Animation animation) {
+
+    }
+}
