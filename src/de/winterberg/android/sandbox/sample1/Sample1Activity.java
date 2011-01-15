70e4aa3e7a144579e1af5956c8ff21300d263dab
Benjamin Winterberg <winterbe@googlemail.com>
2011-01-15 12:19:33 +0100

    add some blur

--- src/de/winterberg/android/sandbox/sample1/Sample1Activity.java
+++ src/de/winterberg/android/sandbox/sample1/Sample1Activity.java
@@ -1,6 +1,7 @@
 package de.winterberg.android.sandbox.sample1;
 
 import android.app.Activity;
+import android.graphics.PixelFormat;
 import android.os.Bundle;
 
 /**
@@ -13,4 +14,9 @@ public class Sample1Activity extends Activity {
         setContentView(new Sample1View(this));
     }
 
+    @Override
+    public void onAttachedToWindow() {
+        super.onAttachedToWindow();
+        getWindow().setFormat(PixelFormat.RGBA_8888);
+    }
 }
--- src/de/winterberg/android/sandbox/sample1/Sample1View.java
+++ src/de/winterberg/android/sandbox/sample1/Sample1View.java
@@ -15,12 +15,53 @@ public class Sample1View extends View {
     private static final String QUOTE = "Now is the time for all good men to come to the aid of their country.";
 
     private Animation animation;
+    private Paint gPaint;
+    private Paint cPaint;
+    private Path glowCircle;
+    private Path circle;
+    private Paint tPaint;
 
     public Sample1View(Context context) {
         super(context);
+        init();
     }
 
-    private void initAnimation(Canvas canvas) {
+    private void init() {
+        gPaint = new Paint();
+        gPaint.setAlpha(255);
+        gPaint.setShadowLayer(40, 0, 0, Color.argb(200, 255, 0, 0));
+
+        cPaint = new Paint();
+        cPaint.setAntiAlias(true);
+        cPaint.setDither(true);
+
+        BlurMaskFilter filter = new BlurMaskFilter(10f, BlurMaskFilter.Blur.INNER);
+        cPaint.setMaskFilter(filter);
+
+
+        int x = 150;
+        int y = 150;
+        int r = 100;
+
+        glowCircle = new Path();
+        glowCircle.addCircle(x, y, r, Path.Direction.CW);
+
+        int color1 = Color.rgb(40, 40, 40);
+        int color2 = Color.rgb(220, 220, 220);
+        LinearGradient gradient = new LinearGradient(0, 0, 0, y*2, color2, color1, Shader.TileMode.REPEAT);
+        cPaint.setShader(gradient);
+
+        circle = new Path();
+        circle.addCircle(x, y, r, Path.Direction.CW);
+
+        tPaint = new Paint();
+        tPaint.setTextSize(18f);
+        tPaint.setTypeface(Typeface.DEFAULT_BOLD);
+        tPaint.setColor(Color.BLACK);
+        tPaint.setAntiAlias(true);
+    }
+
+    private void initAnimation() {
         animation = new RotateAnimation(0, 360, 150, 150);
         animation.setRepeatCount(Animation.INFINITE);
         animation.setRepeatMode(Animation.RESTART);
@@ -32,42 +73,9 @@ public class Sample1View extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         if (animation == null)
-            initAnimation(canvas);
+            initAnimation();
 
-        int x = 150;
-        int y = 150;
-        int r = 100;
-
-        Paint glow = new Paint();
-        glow.setAlpha(255);
-        glow.setShadowLayer(40, 0, 0, Color.argb(200, 255, 0, 0));
-
-        Path glowCircle = new Path();
-        glowCircle.addCircle(x, y, r, Path.Direction.CW);
-        canvas.drawPath(glowCircle, glow);
-
-
-
-        Paint cPaint = new Paint();
-        cPaint.setAntiAlias(true);
-        cPaint.setDither(true);
-
-        int color1 = Color.rgb(60, 60, 60);
-        int color2 = Color.rgb(200, 200, 200);
-        LinearGradient gradient = new LinearGradient(0, 0, 0, y*2, color2, color1, Shader.TileMode.REPEAT);
-        cPaint.setShader(gradient);
-
-
-        Paint tPaint = new Paint();
-        tPaint.setTextSize(18f);
-
-        tPaint.setTypeface(Typeface.DEFAULT_BOLD);
-        tPaint.setColor(Color.BLACK);
-        tPaint.setAntiAlias(true);
-
-        Path circle = new Path();
-        circle.addCircle(x, y, r, Path.Direction.CW);
-
+        canvas.drawPath(glowCircle, gPaint);
         canvas.drawPath(circle, cPaint);
         canvas.drawTextOnPath(QUOTE, circle, 0, 20, tPaint);
     }
