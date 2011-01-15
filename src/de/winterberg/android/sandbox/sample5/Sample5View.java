6d84c9723603c698512000f4f5b4b6b1e9d3fd91
Benjamin Winterberg <winterbe@googlemail.com>
2011-01-15 18:41:13 +0100

    draw some simple paths for collision

--- src/de/winterberg/android/sandbox/sample5/Sample5View.java
+++ src/de/winterberg/android/sandbox/sample5/Sample5View.java
@@ -1,27 +1,81 @@
 package de.winterberg.android.sandbox.sample5;
 
 import android.content.Context;
-import android.view.SurfaceHolder;
-import android.view.SurfaceView;
+import android.graphics.*;
+import android.view.View;
 
 /**
+ * Playing around with collision detection.
+ *
  * @author Benjamin Winterberg
  */
-public class Sample5View extends SurfaceView implements SurfaceHolder.Callback{
+public class Sample5View extends View {
+    public static final String TAG = "Sample5";
+
+    private Paint collisionPaint;
+    private Paint normalPaint;
+
+    private Path circle;
+    private Path triangle;
+    private Path square;
+
 
     public Sample5View(Context context) {
         super(context);
+        initPaints();
+        initPaths();
     }
 
-    public void surfaceCreated(SurfaceHolder surfaceHolder) {
+    private void initPaths() {
+        circle = new Path();
+        circle.addCircle(5, 5, 5, Path.Direction.CW);
 
+        triangle = new Path();
+        triangle.moveTo(0, 0);
+        triangle.lineTo(10, 0);
+        triangle.lineTo(5, 7);
+        triangle.lineTo(0, 0);
+
+        square = new Path();
+        square.addRect(0, 0, 10, 10, Path.Direction.CW);
+
+
+        float sx = 7, sy = 7;
+
+        Matrix matrix = new Matrix();
+
+        matrix.setScale(sx, sy);
+        matrix.preRotate(180);
+        matrix.postTranslate(100, 100);
+        circle.transform(matrix);
+
+        matrix.setScale(sx, sy);
+        matrix.preRotate(180);
+        matrix.postTranslate(200, 200);
+        triangle.transform(matrix);
+
+        matrix.setScale(sx, sy);
+        matrix.preRotate(180);
+        matrix.postTranslate(250, 350);
+        square.transform(matrix);
     }
 
-    public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+    private void initPaints() {
+        normalPaint = new Paint();
+        normalPaint.setColor(Color.WHITE);
+        normalPaint.setStyle(Paint.Style.STROKE);
+        normalPaint.setStrokeCap(Paint.Cap.ROUND);
+        normalPaint.setStrokeJoin(Paint.Join.ROUND);
+        normalPaint.setStrokeWidth(4f);
 
+        collisionPaint = new Paint(normalPaint);
+        collisionPaint.setColor(Color.RED);
     }
 
-    public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
-
+    @Override
+    protected void onDraw(Canvas canvas) {
+        canvas.drawPath(circle, normalPaint);
+        canvas.drawPath(triangle, normalPaint);
+        canvas.drawPath(square, normalPaint);
     }
 }
